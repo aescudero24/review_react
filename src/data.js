9b43950441b69e3bc6dc59bbbdeade771194f04f
aescudero <adrianescudero1305@gmail.com>
2023-10-13 15:33:47 -0500

    made it my own

--- src/data.js
+++ src/data.js
@@ -1,35 +1,34 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
-    job: "web developer",
+    name: "Lionel Messi",
+    job: "La Pulga",
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRWZahGYzkieBfnLHeLjSKPue1S0AokNoqyaZHE6iuKtNFZHd9ZUOkInBXoNg-JGwbSzomWlDB7SHZZ9X0",
+    text: `"La Pulga," or "The Flea," aptly captures Lionel Messi's diminutive stature and his astounding ability to dominate the football pitch with his incredible agility and skills.`,
   },
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
+    name: "Cristiano Ronaldo",
+    job: "Mr. Champions League",
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "https://api.internationalchampionscup.com/v1/storage/9a4a5e67-9842-4a97-a9ac-b186e56b42f1?cx=0&cy=90&cw=2684&ch=1510&w=900&fit=contain",
+    text: `Cristiano Ronaldo, also known as "Mr. Champions League," is a football legend celebrated for his unparalleled success and numerous titles in the UEFA Champions League.`,
   },
   {
     id: 3,
-    name: "peter jones",
-    job: "intern",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    name: "Neymar Jr.",
+    job: "Batman",
+    image: "https://pbs.twimg.com/media/FMs4oRyXoAUcx-8.jpg:large",
+    text: `Neymar, known as "Batman," is a dynamic Brazilian football star who dazzles with his flair, creativity, and caped crusader-like performances on the field.`,
   },
   {
     id: 4,
-    name: "bill anderson",
-    job: "the boss",
+    name: "Kylian Mbappe",
+    job: "Ninja Turtle",
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+      "https://i.kym-cdn.com/entries/icons/original/000/043/087/61262f9363fbf.jpeg",
+    text: `Kylian Mbappé, often likened to a "Ninja Turtle" for his incredible speed, agility, and youthful exuberance, is a French football sensation who leaves defenders in his wake.`,
   },
 ];
 
